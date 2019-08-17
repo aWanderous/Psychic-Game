ab019a76ecdebb9aacfc1bf2969beecbd80e39d1
Alex Wan <a_wanderous@hotmail.com>
2019-08-17 12:43:45 +1000

    fixed correctText bug

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,10 @@
 
 // the counters starting points
+var correct = "";
 var wins = 0;
 var losses = 0;
 var guessCount = 10;
-var guessed = 0;
+
 
 // letters in array
 var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
@@ -21,6 +22,7 @@ wins++;
 guessCount = 10;
 var correctText = document.getElementById("correct-text");
 var reset = document.getElementById("userchoice-text").value = '';
+correctText.textContent = "The magic letter was: " + magicLetter;
 } else { 
   guessCount--;
 }
@@ -30,6 +32,7 @@ losses++;
 guessCount = 10;
 var correctText = document.getElementById("correct-text");
 var reset = document.getElementById("userchoice-text").value = '';
+correctText.textContent = "The magic letter was: " + magicLetter;
 }
 
 // stores the accumlated results
@@ -43,5 +46,4 @@ userChoiceText.textContent = "You guessed: " + userGuess;
 winsText.textContent = "wins: " + wins;
 lossesText.textContent = "losses: " + losses;
 countText.textContent = "Guesses you have left: " + guessCount;
-correctText.textContent = "The magic letter was: " + magicLetter;
 };
